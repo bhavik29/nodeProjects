59ad9208b3bfeef9b114cb24ee51c53763f629ec
Bhavik Thakkar <bhavik.t@lucideustech.com>
2018-10-06 10:16:19 +0530

    Using Yargs Module

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,13 +1,25 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
+const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
-var res = notes.add(9, -2);
+const argv = yargs.argv;
+var command = argv._[0];
 
-console.log(res);
+console.log("Command:",command)
+console.log("Yargs", argv);
 
-// var user = os.userInfo();
-//
-// fs.appendFileSync('greetings.txt', `Hello ${user.username}! You are ${notes.age}`);
+if(command === 'add'){
+  notes.addNote(argv.title, argv.body);
+}else if(command === 'list'){
+  notes.getAll();
+}else if(command === 'remove'){
+  notes.removeNote(argv.title);
+}else if(command === 'read'){
+  notes.readNote(argv.title);
+}else{
+  console.log('Command not recognised');
+}
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,5 +1,24 @@
 console.log('Starting notes.js');
 
-module.exports.add = (a, b) => {
-  return (a + b);            
+var addNote = (title, body) => {
+  console.log('Adding Note', title, body);
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var removeNote = (title) => {
+  console.log('Remove note with title', title);
+};
+
+var readNote = (title) => {
+  console.log('Reading note with title', title);
+}
+
+module.exports = {
+  addNote,
+  getAll,
+  removeNote,
+  readNote
 };
